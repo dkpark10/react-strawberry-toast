8457df5a29e09b68e97882b30a20c804ffa11f60
dkpark10 <dkpark10@naver.com>
2024-11-21 11:18:38 +0900

    feat: init

---
+++ example/src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Toast from 'strawberry-toast';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Toast />
+      <div>app example</div>
+    </React.Fragment>
+  );
+}
