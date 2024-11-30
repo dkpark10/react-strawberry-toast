d54f70fe4203a255d06cf9e4fd8862a9007e8664
dkpark10 <dkpark10@naver.com>
2024-11-30 21:34:12 +0900

    feat: project setting

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ToastContainer, toast } from '../../src';
+
+export default function App() {
+  const click = () => toast('message');
+  return (
+    <React.Fragment>
+      <div>example</div>
+      <ToastContainer />
+      <button onClick={click}>click</button>
+    </React.Fragment>
+  );
+}
---
+++ src/toast.ts
@@ -0,0 +1,3 @@
+export const toast = (message: string) => {
+  console.log(message);
+}
