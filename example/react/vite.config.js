904f2bfaf552bc95f334f12c64e3a4f326e88a0c
dkpark10 <dkpark10@naver.com>
2024-12-16 23:47:08 +0900

    chore: update build option

--- example/react/vite.config.js
+++ example/react/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vitest/config';
-import wyw from '@wyw-in-js/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    wyw({
-      include: ['**/*.{ts,tsx}'],
-      babelOptions: {
-        presets: ['@babel/preset-typescript', '@babel/preset-react'],
-      },
-    }),
   ],
 });
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
+import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig(() => ({
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
+  build: {
+    minify: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: 'index',
+    },
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
