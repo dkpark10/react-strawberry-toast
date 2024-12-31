ec6ba62c33ca4ccb7e32ca73b0680727a0334aa7
dkpark10 <dkpark10@naver.com>
2024-12-31 22:12:28 +0900

    chore: add headless build config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,60 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import dts from 'vite-plugin-dts';
 import svgr from 'vite-plugin-svgr';
-import preserveDirectives from 'rollup-preserve-directives'
+import preserveDirectives from 'rollup-preserve-directives';
 
-export default defineConfig(() => ({
-  plugins: [
-    dts({
-      rollupTypes: true,
-    }),
-    svgr(),
-    preserveDirectives(),
-  ],
-  build: {
-    minify: true,
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'cjs'],
-      fileName: 'index',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  if (env.VITE_HEADLESS === 'true') {
+    return {
+      plugins: [
+        dts({
+          rollupTypes: true,
+        }),
+      ],
+      build: {
+        emptyOutDir: false,
+        minify: true,
+        lib: {
+          entry: path.resolve(__dirname, 'src/hooks/use-strawberry-toast.ts'),
+          formats: ['es', 'cjs'],
+          fileName: 'headless',
+        },
+        outDir: 'dist',
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+        },
+      },
+    };
+  }
+
+  return {
+    plugins: [
+      dts({
+        rollupTypes: true,
+      }),
+      svgr(),
+      preserveDirectives(),
+    ],
+    build: {
+      emptyOutDir: false,
+      minify: true,
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        formats: ['es', 'cjs'],
+        fileName: 'index',
+      },
+      outDir: 'dist',
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+      },
     },
-    outDir: 'dist',
-    rollupOptions: {
-      external: ['react', 'react-dom'],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './setupTest.js',
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTest.js',
-  },
-}));
+  };
+});
