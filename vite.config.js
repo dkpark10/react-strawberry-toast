efc0954c2db663a81dfc98402ca3a3c95bbe0b06
dkpark10 <dkpark10@naver.com>
2025-01-25 22:55:12 +0900

    chore: remove unnecessary packages

--- vite.config.js
+++ vite.config.js
@@ -1,70 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './setupTest.js',
-    },
-  };
-
-  // if (env.VITE_HEADLESS === 'true') {
-  //   return {
-  //     plugins: [
-  //       dts({
-  //         rollupTypes: true,
-  //         beforeWriteFile: (typeFilePath) => {
-  //           if (typeFilePath === path.resolve(__dirname, 'dist/index.d.ts')) {
-  //             const targetPath = path.resolve(__dirname, 'dist/headless.d.ts');
-  //             copyFileSync(typeFilePath, targetPath);
-  //           }
-  //         },
-  //       }),
-  //     ],
-  //     build: {
-  //       emptyOutDir: false,
-  //       minify: true,
-  //       lib: {
-  //         entry: path.resolve(__dirname, 'src/hooks/use-toast.ts'),
-  //         formats: ['es', 'cjs'],
-  //         fileName: 'headless',
-  //       },
-  //       outDir: 'dist',
-  //       rollupOptions: {
-  //         external: ['react', 'react-dom'],
-  //       },
-  //     },
-  //   };
-  // }
-
-  // return {
-  //   plugins: [
-  //     dts({
-  //       rollupTypes: true,
-  //     }),
-  //     svgr(),
-  //     preserveDirectives(),
-  //   ],
-  //   build: {
-  //     emptyOutDir: false,
-  //     minify: true,
-  //     lib: {
-  //       entry: path.resolve(__dirname, 'src/index.ts'),
-  //       formats: ['es', 'cjs'],
-  //       fileName: 'index',
-  //     },
-  //     outDir: 'dist',
-  //     rollupOptions: {
-  //       external: ['react', 'react-dom'],
-  //     },
-  //   },
-  //   test: {
-  //     globals: true,
-  //     environment: 'jsdom',
-  //     setupFiles: './setupTest.js',
-  //   },
-  // };
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTest.js',
+  },
 });
